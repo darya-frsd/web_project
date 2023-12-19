dc872ed70ccef1c72a14579d978e4136e6d60d5a
darya-frsd <darya.frsd@gmail.com>
2023-12-19 03:09:09 +0300

    Menu changes

--- scripts/articles.js
+++ scripts/articles.js
@@ -3,3 +3,15 @@ function scrollAds(direction) {
     const scrollAmount = direction * container.clientWidth;
     container.scrollLeft += scrollAmount;
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const tabsContainer = document.querySelector('.tabs-container');
+    const tabs = tabsContainer.querySelectorAll('button');
+
+    tabs.forEach(tab => {
+        tab.addEventListener('click', () => {
+            tabs.forEach(t => t.classList.remove('active-tab'));
+            tab.classList.add('active-tab');
+        });
+    });
+});
